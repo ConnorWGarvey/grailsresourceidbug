182dbab8b4f4d8e073f1a5d56163a2e358b59a9e
Connor Garvey <garvey@apple.com>
2016-06-14 08:14:06 -0700

    Added a failing test for gets

--- src/test/groovy/id/bug/ExampleSpec.groovy
+++ src/test/groovy/id/bug/ExampleSpec.groovy
@@ -4,13 +4,15 @@ import static org.springframework.http.HttpStatus.UNPROCESSABLE_ENTITY
 import grails.plugins.rest.client.RestBuilder
 import grails.test.mixin.integration.Integration
 import spock.lang.Specification
+import spock.lang.Unroll
 
 /**
  * See the API for {@link grails.test.mixin.domain.DomainClassUnitTestMixin} for usage instructions
  */
-@Integration class ExampleSpec extends Specification {
-  void 'the test'() {
-    when: def response = new RestBuilder().delete('http://localhost:8080/examples/notavalidid')
+@Integration @Unroll class ExampleSpec extends Specification {
+  def 'invalid ID for #method'() {
+    when: def response = new RestBuilder()."$method"('http://localhost:8080/examples/notavalidid')
     then: response.statusCode == UNPROCESSABLE_ENTITY
+    where: method << ['delete', 'get']
   }
 }
